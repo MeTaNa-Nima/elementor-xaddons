1b401ad74d4cebfa8ed7e05059284f084d6ebbb4
Nima Amani <metananima@gmail.com>
2024-02-11 17:12:01 +0330

    v1.0.0 release.

--- elementor-xaddons.php
+++ elementor-xaddons.php
@@ -4,7 +4,7 @@
  * Plugin Name: Elementor Header and Text Widget
  * Description: Custom Elementor widget combining a header and text box.
  * Version: 1.0.0
- * Author: Nima Amani <metananima@gmail.com>
+ * Author: Nima Amani MeTaNa <metananima@gmail.com>
  */
 
 define('XADDONS_PLUGIN_URL', plugin_dir_url(__FILE__));
