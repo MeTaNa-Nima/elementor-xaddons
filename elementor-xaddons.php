05d368c7c256c90297c38c967e97f40576b8b997
Nima Amani <metananima@gmail.com>
2024-02-14 15:41:10 +0330

    Header, Subheader and text fields added

--- elementor-xaddons.php
+++ elementor-xaddons.php
@@ -1,10 +1,18 @@
 <?php
 
-/**
- * Plugin Name: Elementor Header and Text Widget
- * Description: Custom Elementor widget combining a header and text box.
- * Version: 1.0.1
- * Author: Nima Amani MeTaNa <metananima@gmail.com>
+
+/*
+ * Plugin Name:       Elementor Header and Text Widget
+ * Plugin URI:        https://github.com/MeTaNa-Nima/elementor-xaddons/
+ * Description:       Custom Elementor widget combining a header and text box.
+ * Version:           1.0.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Nima Amani MeTaNa <metananima@gmail.com>
+ * Author URI:        https://nimaamani.ir/en/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://github.com/MeTaNa-Nima/elementor-xaddons/
  */
 
 define('XADDONS_PLUGIN_URL', plugin_dir_url(__FILE__));
